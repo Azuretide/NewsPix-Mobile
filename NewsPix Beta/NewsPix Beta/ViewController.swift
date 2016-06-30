c113423cca8de288f0fcd0070c61f86cc116d459
Azuretide <Azuretide@gmail.com>
2016-06-29 22:40:56 -0400

    Updated app with zoom functionality
    Has some ux oddities to resolve

--- NewsPix Beta/NewsPix Beta/ViewController.swift
+++ NewsPix Beta/NewsPix Beta/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var swipeRight: UISwipeGestureRecognizer!
     @IBOutlet var swipeLeft: UISwipeGestureRecognizer!
-    @IBOutlet weak var pictureTitle: UILabel!
-    @IBOutlet weak var display: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pictureTitle: UIButton!
+    
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
     var index = 0
+    var lastZoomScale: CGFloat = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +30,14 @@ class ViewController: UIViewController {
         
         //Download content from server
 //        altParseJSON(getData("http://localhost:5000/get_all_stories")!)
-        let numberOfCalls = 5
+        let numberOfCalls = 8
         for _ in 1...numberOfCalls {
                 parseJSON(getData("http://dev.newspix.today/random_story")!)
         }
 
         //Initialize Display
-        self.pictureTitle.text = names[self.index]
-        self.display.contentVerticalAlignment = UIControlContentVerticalAlignment.Center;
-        self.display.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Center;
-        self.display.imageView?.contentMode = UIViewContentMode.ScaleAspectFit;
-        self.display.setImage(images[self.index], forState: UIControlState.Normal)
+        self.pictureTitle.setTitle(names[self.index],forState: UIControlState.Normal)
+        self.imageView.image = images[self.index]
 
         //Initialize list of read headlines if save data exists, otherwise set to empty read array
         if let archive = userDefaults.arrayForKey("Seen") {
@@ -58,6 +63,11 @@ class ViewController: UIViewController {
         //Get number of unread stories by comparing lengths of names, read
         let unread = names.count - read.count
         print(unread)
+        
+        scrollView.delegate = self
+        updateZoom()
+        updateConstraints()
+
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -80,12 +90,12 @@ class ViewController: UIViewController {
             self.index += 1
             if self.index > names.count-1 {self.index = 0}
         }
-        self.display.fadeOut()
+        self.imageView.fadeOut()
         self.pictureTitle.fadeOut()
-        self.display.setImage(images[self.index], forState: UIControlState.Normal)
-        self.pictureTitle.text = names[self.index]
+        self.imageView.image = images[self.index]
+        self.pictureTitle.setTitle(names[self.index],forState: UIControlState.Normal)
         self.pictureTitle.fadeIn()
-        self.display.fadeIn()
+        self.imageView.fadeIn()
         
         //Update read stories
         if (read.indexOf(names[self.index]) == -1) {
@@ -93,7 +103,7 @@ class ViewController: UIViewController {
             userDefaults.setObject(read, forKey: "Seen")
         }
     }
-    @IBAction func didPressDisplay() {
+    @IBAction func didPressTitle() {
         let url = urls[self.index];
         UIApplication.sharedApplication().openURL(url);
     }
@@ -107,12 +117,12 @@ class ViewController: UIViewController {
             self.index += 1
             if self.index > names.count-1 {self.index = 0}
         }
-        self.display.fadeOut()
+        self.imageView.fadeOut()
         self.pictureTitle.fadeOut()
-        self.display.setImage(images[self.index], forState: UIControlState.Normal)
-        self.pictureTitle.text = names[self.index]
+        self.imageView.image = images[self.index]
+        self.pictureTitle.setTitle(names[self.index],forState: UIControlState.Normal)
         self.pictureTitle.fadeIn()
-        self.display.fadeIn()
+        self.imageView.fadeIn()
         
         //Update read stories
         if (read.indexOf(names[self.index]) == -1) {
@@ -122,7 +132,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func shareButtonClicked(sender: UIBarButtonItem) {
-        let textToShare = self.pictureTitle.text!
+        let textToShare = self.pictureTitle.currentTitle!
         let myWebsite = urls[self.index]
         let objectsToShare = [textToShare, myWebsite]
         let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
@@ -143,5 +153,69 @@ class ViewController: UIViewController {
         self.presentViewController(activityVC, animated: true, completion: nil)
         }
     
-    } //Do not delete! End of ViewController class
+    override func viewWillTransitionToSize(size: CGSize,
+        withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+            
+            super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+            
+            coordinator.animateAlongsideTransition({ [weak self] _ in
+                self?.updateZoom()
+                }, completion: nil)
+    }
+    
+    func updateConstraints() {
+        if let image = imageView.image {
+            let imageWidth = image.size.width
+            let imageHeight = image.size.height
+            
+            let viewWidth = scrollView.bounds.size.width
+            let viewHeight = scrollView.bounds.size.height
+            
+            // center image if it is smaller than the scroll view
+            var hPadding = (viewWidth - scrollView.zoomScale * imageWidth) / 2
+            if hPadding < 0 { hPadding = 0 }
+            
+            var vPadding = (viewHeight - scrollView.zoomScale * imageHeight) / 2
+            if vPadding < 0 { vPadding = 0 }
+            
+            imageViewTrailingConstraint.constant = hPadding
+            imageViewLeadingConstraint.constant = hPadding
+            
+            imageViewTopConstraint.constant = vPadding
+            imageViewBottomConstraint.constant = vPadding
+            
+            view.layoutIfNeeded()
+        }
+    }
+    
+    // Zoom to show as much image as possible unless image is smaller than the scroll view
+    private func updateZoom() {
+        if let image = imageView.image {
+            var minZoom = min(scrollView.bounds.size.width / image.size.width,
+                scrollView.bounds.size.height / image.size.height)
 
+            if minZoom > 1 { minZoom = 1 }
+
+            
+            scrollView.minimumZoomScale = 1
+            scrollView.maximumZoomScale = 10
+            
+            // Force scrollViewDidZoom fire if zoom did not change
+            if minZoom == lastZoomScale { minZoom += 0.000001 }
+            scrollView.zoomScale = minZoom
+            lastZoomScale = scrollView.zoomScale
+        }
+    }
+    
+    // UIScrollViewDelegate
+    // -----------------------
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateConstraints()
+        }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+        }
+    
+    } //Do not delete! End of ViewController class
